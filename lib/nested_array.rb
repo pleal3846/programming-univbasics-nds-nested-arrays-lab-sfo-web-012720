cadaedba0ef9e6142b278dc0a7da6e626580f56d
patrick leal <pjleal3846@icloud.com>
2020-01-18 22:50:21 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -33,10 +33,12 @@ def sorted_matrix
 end
 
 def matrix_lookup(matrix, row, column)
-  assembled_matrix [1][0]
-  sorted_matrix[1][0]
+  assembled_matrix [1][1]
+  sorted_matrix[1][1]
   # Given any matrix (array of arrays), a row index and a column index, 
   # Return the matrix's content at that row and and column
+  return assembled_matrix
+  return sorted_matrix
 end
 
 def matrix_update(matrix, row, column, new_value)
